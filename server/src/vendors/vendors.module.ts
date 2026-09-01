678d7e783d469320c77e4f8d8bc499d615b4feb1
stedowaisalishah <alishahowais@gmail.com>
2026-09-01 23:17:34 +0500

    feat(vendors): import report statement support

--- server/src/vendors/vendors.module.ts
+++ server/src/vendors/vendors.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { VendorsService } from './vendors.service';
 import { VendorsController } from './vendors.controller';
+import { ReportsModule } from '../reports/reports.module';
 
 @Module({
+  imports: [ReportsModule],
   controllers: [VendorsController],
   providers: [VendorsService],
   exports: [VendorsService],
